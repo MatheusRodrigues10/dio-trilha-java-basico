4be42ee61c3a665ccdf72486a3478d2605237926
Matheus Rodrigues <matheushehe007@gmail.com>
2025-04-11 11:00:59 -0300

    Desafio dio completo

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,6 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Hello, World!");
+        System.out.println("Por favor, digite o número da Agência:");
+        int agencia =  scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        String numero = scanner.next();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String cliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + cliente + " obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " +  numero  + " e seu saldo: " + saldo + " já está disponível para saque");
     }
 }
